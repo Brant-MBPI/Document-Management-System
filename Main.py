56f41e967c9b271510463200b8b983b51be80d38
Freyze10 <bjabillanoza@gmail.com>
2025-09-02 21:45:25 +0800

    fixed venv and updated msds form (section 11)

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import sys
 
 from PyQt6.QtCore import Qt
@@ -6,12 +6,12 @@ from PyQt6.QtGui import QPixmap, QIcon
 
 from db import db_con
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QWidget, QTabWidget, \
-    QTableWidget, QLineEdit, QHeaderView, QTableWidgetItem
+    QTableWidget, QLineEdit, QHeaderView, QTableWidgetItem, QScrollArea, QTextEdit
 import msds_data_entry
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        db_con.create_tables()
+        # db_con.create_tables()
 
         self.main_layout = QVBoxLayout()
         self.main_tabs = QTabWidget()
@@ -24,17 +24,26 @@ class MainWindow(QMainWindow):
         self.section6_layout = QVBoxLayout()
         self.section7_layout = QVBoxLayout()
         self.section8_layout = QVBoxLayout()
+        self.section9_layout = QVBoxLayout()
+        self.section10_layout = QVBoxLayout()
+        self.section11_layout = QVBoxLayout()
+        self.section12_layout = QVBoxLayout()
+        self.section13_layout = QVBoxLayout()
+        self.section14_layout = QVBoxLayout()
+        self.section15_layout = QVBoxLayout()
+        self.section16_layout = QVBoxLayout()
+        self.section17_layout = QVBoxLayout()
 
 
         # MSDS FORM init
             #Section 1
         self.trade_label_input = QLineEdit()
-        self.manufactured_label_input = QLineEdit()
+        self.manufactured_label_input = QTextEdit()
         self.tel_label_input = QLineEdit()
         self.facsimile_label_input = QLineEdit()
         self.email_label_input = QLineEdit()
             #Section2
-        self.composition_input = QLineEdit()
+        self.composition_input = QTextEdit()
             #Section3
         self.hazard_preliminaries_input = QLineEdit()
         self.hazard_entry_route_input = QLineEdit()
@@ -48,9 +57,9 @@ class MainWindow(QMainWindow):
         self.first_aid_skin_input = QLineEdit()
         self.first_aid_ingestion_input = QLineEdit()
             #Section5
-        self.fire_fighting_media_input = QLineEdit()
+        self.fire_fighting_media_input = QTextEdit()
             #Section6
-        self.accidental_release_input = QLineEdit()
+        self.accidental_release_input = QTextEdit()
             #Section7
         self.handling_input = QLineEdit()
         self.storage_input = QLineEdit()
@@ -60,7 +69,20 @@ class MainWindow(QMainWindow):
         self.hand_protection_input = QLineEdit()
         self.eye_protection_input = QLineEdit()
         self.skin_protection_input = QLineEdit()
-
+            #Section9
+        self.appearance_input = QLineEdit()
+        self.odor_input = QLineEdit()
+        self.heat_stability_input = QLineEdit()
+        self.light_fastness_input = QLineEdit()
+        self.decomposition_input = QLineEdit()
+        self.flash_point_input = QLineEdit()
+        self.auto_ignition_input = QLineEdit()
+        self.explosion_property_input = QLineEdit()
+        self.solubility_input = QLineEdit()
+            #Section10
+        self.stability_reactivity_input = QTextEdit()
+            #Section11
+        self.toxicological_input = QTextEdit()
 
 
         self.msds_tab = QWidget()           #MSDS Main Tab
@@ -127,16 +149,36 @@ class MainWindow(QMainWindow):
         self.msds_records_layout = QVBoxLayout(self.msds_records_tab)  # inside MSDS sub-tab Records
         self.msds_records_layout.addWidget(self.msds_records_table)
 
-        self.msds_data_entry_layout = QVBoxLayout(self.msds_data_entry_tab)  # inside MSDS sub-tab Data Entry
-        self.msds_data_entry_layout.addLayout(self.section1_layout)
-        self.msds_data_entry_layout.addLayout(self.section2_layout)
-        self.msds_data_entry_layout.addLayout(self.section3_layout)
-        self.msds_data_entry_layout.addLayout(self.section4_layout)
-        self.msds_data_entry_layout.addLayout(self.section5_layout)
-        self.msds_data_entry_layout.addLayout(self.section6_layout)
-        self.msds_data_entry_layout.addLayout(self.section7_layout)
-        self.msds_data_entry_layout.addLayout(self.section8_layout)
+        scroll_area = QScrollArea(self.msds_data_entry_tab)
+        scroll_area.setWidgetResizable(True)
 
+        # Form container inside the scroll area
+        form_container = QWidget()
+        form_layout = QVBoxLayout(form_container)
+
+        form_layout.addLayout(self.section1_layout)
+        form_layout.addLayout(self.section2_layout)
+        form_layout.addLayout(self.section3_layout)
+        form_layout.addLayout(self.section4_layout)
+        form_layout.addLayout(self.section5_layout)
+        form_layout.addLayout(self.section6_layout)
+        form_layout.addLayout(self.section7_layout)
+        form_layout.addLayout(self.section8_layout)
+        form_layout.addLayout(self.section9_layout)
+        form_layout.addLayout(self.section10_layout)
+        form_layout.addLayout(self.section11_layout)
+        form_layout.addLayout(self.section12_layout)
+        form_layout.addLayout(self.section13_layout)
+        form_layout.addLayout(self.section14_layout)
+        form_layout.addLayout(self.section15_layout)
+        form_layout.addLayout(self.section16_layout)
+        form_layout.addLayout(self.section17_layout)
+
+        scroll_area.setWidget(form_container)
+
+        # Final layout for the tab
+        self.msds_data_entry_layout = QVBoxLayout(self.msds_data_entry_tab)
+        self.msds_data_entry_layout.addWidget(scroll_area)
         self.coa_records_layout = QVBoxLayout(self.coa_records_tab)  # inside COA sub-tab Records
         self.coa_records_layout.addWidget(QLabel("Records"))
 
@@ -158,6 +200,9 @@ class MainWindow(QMainWindow):
         msds_data_entry.form_section6(self)
         msds_data_entry.form_section7(self)
         msds_data_entry.form_section8(self)
+        msds_data_entry.form_section9(self)
+        msds_data_entry.form_section10(self)
+        msds_data_entry.form_section11(self)
 
 
     def load_msds_table(self):
@@ -214,220 +259,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
-import sys
-
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPixmap, QIcon
-
-from db import db_con
-from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QLabel, QWidget, QTabWidget, \
-    QTableWidget, QLineEdit, QHeaderView, QTableWidgetItem
-import msds_data_entry
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        db_con.create_tables()
-
-        self.main_layout = QVBoxLayout()
-        self.main_tabs = QTabWidget()
-
-        self.section1_layout = QVBoxLayout()
-        self.section2_layout = QVBoxLayout()
-        self.section3_layout = QVBoxLayout()
-        self.section4_layout = QVBoxLayout()
-        self.section5_layout = QVBoxLayout()
-        self.section6_layout = QVBoxLayout()
-        self.section7_layout = QVBoxLayout()
-        self.section8_layout = QVBoxLayout()
-
-
-        # MSDS FORM init
-            #Section 1
-        self.trade_label_input = QLineEdit()
-        self.manufactured_label_input = QLineEdit()
-        self.tel_label_input = QLineEdit()
-        self.facsimile_label_input = QLineEdit()
-        self.email_label_input = QLineEdit()
-            #Section2
-        self.composition_input = QLineEdit()
-            #Section3
-        self.hazard_preliminaries_input = QLineEdit()
-        self.hazard_entry_route_input = QLineEdit()
-        self.hazard_symptoms_input = QLineEdit()
-        self.hazard_restrictive_condition_input = QLineEdit()
-        self.hazard_eyes_input = QLineEdit()
-        self.hazard_general_note_input = QLineEdit()
-            #Section4
-        self.first_aid_inhalation_input = QLineEdit()
-        self.first_aid_eyes = QLineEdit()
-        self.first_aid_skin_input = QLineEdit()
-        self.first_aid_ingestion_input = QLineEdit()
-            #Section5
-        self.fire_fighting_media_input = QLineEdit()
-            #Section6
-        self.accidental_release_input = QLineEdit()
-            #Section7
-        self.handling_input = QLineEdit()
-        self.storage_input = QLineEdit()
-            #Section8
-        self.exposure_control_input = QLineEdit()
-        self.respiratory_protection_input = QLineEdit()
-        self.hand_protection_input = QLineEdit()
-        self.eye_protection_input = QLineEdit()
-        self.skin_protection_input = QLineEdit()
-
-
-
-        self.msds_tab = QWidget()           #MSDS Main Tab
-        self.msds_layout = QVBoxLayout(self.msds_tab)
-
-        self.msds_sub_tabs = QTabWidget()
-        self.msds_layout.addWidget(self.msds_sub_tabs)
-
-        self.msds_data_entry_tab = QWidget()
-        self.msds_records_tab = QWidget()
-
-        self.coa_tab = QWidget()  # COA Main Tab
-        self.coa_layout = QVBoxLayout(self.coa_tab)
-
-        self.coa_sub_tabs = QTabWidget()
-        self.coa_layout.addWidget(self.coa_sub_tabs)
-
-        self.coa_data_entry_tab = QWidget()
-        self.coa_records_tab = QWidget()
-
-        # SUB-TAB
-        self.msds_sub_tabs.addTab(self.msds_records_tab, "Records")
-        self.msds_sub_tabs.addTab(self.msds_data_entry_tab, "Data Entry")
-
-        self.coa_sub_tabs.addTab(self.coa_records_tab, "Records")
-        self.coa_sub_tabs.addTab(self.coa_data_entry_tab, "Data Entry")
-
-        self.main_tabs.addTab(self.msds_tab, "MSDS")
-        self.main_tabs.addTab(self.coa_tab, "CoA")
-
-        self.search_bar = QLineEdit()
-        self.search_bar.setPlaceholderText("Search...")
-
-        self.main_tabs.setCornerWidget(self.search_bar)
-
-        self.msds_records_table = QTableWidget()   # MSDS Record Table
-        self.msds_records_table.setColumnCount(4)
-        self.msds_records_table.setHorizontalHeaderLabels(["Name", "", "", ""])
-        self.msds_records_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Fixed)
-
-        # Override resize event
-        self.msds_records_table.resizeEvent = self.resize_columns
-        self.msds_records_table.verticalHeader().setDefaultSectionSize(40)
-        self.msds_records_table.horizontalHeader().setFixedHeight(40)
-        self.msds_records_table.setShowGrid(False)
-        self.msds_records_table.setStyleSheet("""
-            QTableWidget::item {
-                border-right: none;
-                border-bottom: 1px solid lightgray;
-                padding-left: 10px;
-                height: 36px;
-            }
-            QTableWidget::item:selected {
-                background-color: #3399FF;           
-                color: black;                         
-            }
-            QHeaderView::section {
-                border-right: none;                  
-                border-bottom: 1px solid lightgray; 
-                background-color: #f0f0f0;         
-            }
-        """)
-
-        self.msds_records_layout = QVBoxLayout(self.msds_records_tab)  # inside MSDS sub-tab Records
-        self.msds_records_layout.addWidget(self.msds_records_table)
-
-        self.msds_data_entry_layout = QVBoxLayout(self.msds_data_entry_tab)  # inside MSDS sub-tab Data Entry
-        self.msds_data_entry_layout.addLayout(self.section1_layout)
-        self.msds_data_entry_layout.addLayout(self.section2_layout)
-        self.msds_data_entry_layout.addLayout(self.section3_layout)
-        self.msds_data_entry_layout.addLayout(self.section4_layout)
-        self.msds_data_entry_layout.addLayout(self.section5_layout)
-        self.msds_data_entry_layout.addLayout(self.section6_layout)
-        self.msds_data_entry_layout.addLayout(self.section7_layout)
-        self.msds_data_entry_layout.addLayout(self.section8_layout)
-
-        self.coa_records_layout = QVBoxLayout(self.coa_records_tab)  # inside COA sub-tab Records
-        self.coa_records_layout.addWidget(QLabel("Records"))
-
-        self.coa_data_entry_layout = QVBoxLayout(self.coa_data_entry_tab)  # inside COA sub-tab Data Entry
-        self.coa_data_entry_layout.addWidget(QLabel("Data Entry"))
-
-        self.main_layout.addWidget(self.main_tabs)
-
-        container = QWidget()
-        container.setLayout(self.main_layout)
-        self.setCentralWidget(container)
-
-        self.load_msds_table()
-        msds_data_entry.form_section1(self)
-        msds_data_entry.form_section2(self)
-        msds_data_entry.form_section3(self)
-        msds_data_entry.form_section4(self)
-        msds_data_entry.form_section5(self)
-        msds_data_entry.form_section6(self)
-        msds_data_entry.form_section7(self)
-        msds_data_entry.form_section8(self)
-
-
-    def load_msds_table(self):
-        self.msds_records_table.insertRow(0)
-        self.msds_records_table.setItem(0, 0, self.create_readonly_item("JDS WA17857E MSDS TDS 08-22-25"))
-        self.msds_records_table.setItem(0, 1, self.create_readonly_item(icon_path="img/view_icon.png", selectable=False))
-        self.msds_records_table.setItem(0, 2, self.create_readonly_item(icon_path="img/delete_icon.png", selectable=False))
-        self.msds_records_table.setItem(0, 3, self.create_readonly_item(icon_path="img/print_icon.png", selectable=False))
-
-    def resize_columns(self, event):
-        total_width = self.msds_records_table.viewport().width()
-
-        # first column = 70%
-        first_col_width = int(total_width * 0.9)
-        self.msds_records_table.setColumnWidth(0, first_col_width)
-
-        # remaining columns share 30%
-        remaining_width = total_width - first_col_width
-        other_cols = self.msds_records_table.columnCount() - 1
-        if other_cols > 0:
-            each_width = int(remaining_width / other_cols)
-            for col in range(1, self.msds_records_table.columnCount()):
-                self.msds_records_table.setColumnWidth(col, each_width)
-
-        super(QTableWidget, self.msds_records_table).resizeEvent(event)
-
-    def create_readonly_item(self, text=None, icon_path=None, selectable=True):
-        item = QTableWidgetItem()
-
-        if text is not None:
-            item.setText(text)
-
-        if icon_path is not None:
-            item.setIcon(QIcon(icon_path))
-
-        # Make the item read-only
-        item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
-
-        if not selectable:
-            # Remove the selectable flag but keep enabled to accept clicks
-            item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEnabled)
-
-        return item
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.setWindowTitle("Document Management System")
-    window.resize(1000, 800)
-    window.showMaximized()
-    sys.exit(app.exec())
-
-
-if __name__ == '__main__':
-    main()
->>>>>>> 4b2f374a4a5590bcfa2a0d63226a93905a83d47a
